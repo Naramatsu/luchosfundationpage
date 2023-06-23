109d99a4ec9e8c2bdfeaadd1394b919bb8385fd3
Naramatsu <joncyy_19@hotmail.com>
2023-06-23 14:56:53 -0500

    added home page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./components/Header/Header";
+import Banner from "./components/Banner/Banner";
+import Sections from "./components/Sections/Sections";
+import SectionInfo from "./components/SectionInfo/SectionInfo";
 
-function App() {
+import "./App.style.scss";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="App kromac-scroll">
+      <Header />
+      <Banner />
+      <Sections />
+      <SectionInfo />
+    </main>
   );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "kromac-ui-18/src/css/style.css";
+import { BrowserRouter as Router } from "react-router-dom";
+import BuildingPage from "./components/BuildingPage/BuildingPage";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+const { REACT_APP_IS_BUILDING_APP } = process.env;
+const isBuildingApp = REACT_APP_IS_BUILDING_APP;
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>{isBuildingApp ? <BuildingPage /> : <App />}</Router>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
