61cfe7a774a78963fae88031857e776fd48e9cd3
Naramatsu <joncyy_19@hotmail.com>
2023-06-29 11:11:14 -0500

    added mixins and style variables

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import BuildingPage from "./components/BuildingPage/BuildingPage";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 const { REACT_APP_IS_BUILDING_APP } = process.env;
-const isBuildingApp = REACT_APP_IS_BUILDING_APP;
+const isBuildingApp = !REACT_APP_IS_BUILDING_APP;
 
 root.render(
   <React.StrictMode>
